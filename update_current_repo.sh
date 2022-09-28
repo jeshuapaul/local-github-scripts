a08b3d8d15a485385860511fa5b76a90e3675abe
root <root@AV-PC-L109.localdomain>
2022-09-28 14:52:19 +0200

    repo name changes

--- update_current_repo.sh
+++ update_current_repo.sh
@@ -54,12 +54,16 @@ repo_name=$(cat .git/config | grep url | awk '{print $3}')
 echo "Updates are going to be pushed to - $repo_name"
 echo "Is this correct ? (y/n)"
 read answer
-if [ "$answer" == "y" ] || [ "$answer" == "Y"]; then
-sudo git remote add origin "https://github.com/jeshuapaul/$repo_name.git"
-else
-	echo "You need to acknowledge the repo name."
-fi
-
+while [ "$answer" != "y" ] || [ "$answer" != "Y" ]; do
+	if [ "$answer" == "y" ] || [ "$answer" == "Y" ]; then
+	sudo git remote add origin "https://github.com/jeshuapaul/$repo_name.git"
+	else
+		echo "You need to acknowledge the repo name."
+		echo "Updates are going to be pushed to - $repo_name"
+		echo "Is this correct ? (y/n)"
+		read answer
+	fi
+done
 # This does some git pull and git push magic, to ensure that the contents of your new Github repository, and the folder on you local system are the same.
 sudo git remote -v
 
